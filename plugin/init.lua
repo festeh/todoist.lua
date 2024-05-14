cecdd814f5d2efee23028a75b42ea227e3136763
Dima Lipin <dimich3d@ya.ru>
2024-05-14 21:02:38 +0200

    force clean

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,5 +1,10 @@
 vim.api.nvim_create_user_command('Todoist',
   function(opts)
+    for pack, _ in pairs(package.loaded) do
+      if pack:match("^todoist") then
+        package.loaded[pack] = nil
+      end
+    end
     require('todoist').main()
   end,
   { desc = "Launch Todoist app", force = true })
